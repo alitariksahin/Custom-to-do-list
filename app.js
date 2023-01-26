a2ee619f0db23b76bc2220c762b63f4a29304eb9
alitariksahin <alitariksah@gmail.com>
2023-01-26 10:57:01 +0300

    async fncs handled

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const router = require("./routes/index");
 
 const app = express();
+
+mongoose.set("strictQuery", false);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use("", router);
 
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log("server started");
-})
+const start = async function() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    app.listen(process.env.PORT || 3000, function() {
+      console.log("server started");
+    });
+  }catch(err) {
+    console.log(err);
+  }
+}
+start();
--- controllers/controllers.js
+++ controllers/controllers.js
@@ -3,9 +3,6 @@ const date = require("../Date.js");
 const _ = require("lodash");
 require("dotenv").config();
 
-mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGO_URI);
-
 const itemSchema = new mongoose.Schema({
   name: String
 });
@@ -31,88 +28,88 @@ const listSchema = new mongoose.Schema({
 
 const List = new mongoose.model("list", listSchema);
 
-const getData = function(req, res) {
+const getData = async function(req, res) {
   let day = date.getDate();
-
-  Item.find({}, function(err, result) {
+  try {
+    const result = await Item.find({});
     if (result.length === 0) {
-      Item.insertMany(items, function(err) {
-        if (err) {
-          console.log(err);
-        }
-        else {
-          console.log("items are inserted succesfully.")
-        }
-      });
+      await Item.insertMany(items);
+      console.log("items are inserted succesfully.");
       res.redirect("/");
     }
-    res.render("list", {list: day, newItems: result});
-  });
+    else {
+      res.render("list", {list: day, newItems: result});
+    }
+  }catch(err) {
+    console.log(err);
+  }
 }
 
-const postData = function(req, res) {
+const postData = async function(req, res) {
   let itemName = req.body.input;
   let titleName = req.body.submit;
 
   const newItem = new Item({
     name: itemName
   });
+  try {
+    if (titleName === date.getDate()) {
+      await newItem.save();
+      res.redirect("/");
+    }
+    else {
+      const result = await List.findOne({name: titleName});
+      result.items.push(newItem);
+      await result.save();
+      res.redirect("/" + titleName);
 
-  if (titleName === date.getDate()) {
-    newItem.save();
-    res.redirect("/");
+    }
+  }catch(err) {
+    console.log(err);
   }
-  else {
-    List.findOne({name: titleName}, function(err, result) {
-      if (!err) {
-        result.items.push(newItem);
-        result.save();
-        res.redirect("/" + titleName);
-      }
-    });
-  }
+
 
 }
 
-const deleteData = function(req, res) {
+const deleteData = async function(req, res) {
   const itemID = req.body.checkbox;
   const listName = req.body.listName;
+  try {
+    if (listName === date.getDate()) {
+      await Item.findOneAndDelete({_id : itemID});
+      res.redirect("/");
+    }
+    else {
+      const result = await List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: itemID}}});
+      res.redirect("/" + listName);
 
-  if (listName === date.getDate()) {
-    Item.findOneAndDelete({_id : itemID}, function(err) {
-        if (err) {
-          console.log(err);
-        }
-        res.redirect("/");
-    });
+    }
+  }catch(err) {
+    console.log(err);
   }
-  else {
-    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: itemID}}}, function(err, result) {
-      if(!err) {
-        res.redirect("/" + listName);
-      }
-    });
-  }
+
 }
 
-const listData = function(req, res) {
+const listData = async function(req, res) {
   const listName = _.capitalize(req.params.listName);
-  List.findOne({name: listName}, function(err, result) {
-    if (!err) {
+  try {
+    const result = await List.findOne({name: listName});
       if (result === null) {
-
         const customList = new List({
           name: listName,
           items: items
         });
-        customList.save();
+        await customList.save();
         res.redirect("/" + listName);
       }
       else {
         res.render("list", {list: listName, newItems: result.items})
       }
-    }
-  })
+
+  }catch(err) {
+    console.log(err);
+  }
+
 }
 
 module.exports = {
